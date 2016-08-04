e563bc28db7934f745b815a68e3f91ad1d646d10
Håvard Pettersson <mail@haavard.me>
2016-08-04 22:50:06 +0200

    Add palindrome test

---
+++ tests/programs/palindrome.fish
@@ -0,0 +1,8 @@
+i:0(?;:a=?v::::"A"(?~"z")?~"Z")$"a"(=?~
+          ~
+     v?(2l<v?%" "-{
+     >1nvn0<
+        >l?vao00.
+        /  \~
+
+Modified from https://fishlanguage.com/playground/q9k9SNvwDtXoFRCZ2
