183f10c20a94372243a0fd2ed7f29891bf350576
Håvard Pettersson <mail@haavard.me>
2016-08-07 20:35:55 +0200

    Clean up test script; improve error output

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,22 +4,44 @@ set -o nounset
 shopt -s nullglob
 shopt -s globstar
 
+# command-line arguments
 test_target=$1
 tests_dir=$2
 
+# ANSI escape strings
 c_red='\e[31m'
 c_green='\e[32m'
-bold='\e[1m'
+c_bold='\e[1m'
 c_clear='\e[0m'
 
+# files used to capture output
 stdout=$(mktemp)
 stderr=$(mktemp)
-empty=$(mktemp)
 
+# the time to run a test before killing it
+timeout_after=0.5
+
+# helper for comparing stdout/stderr data
+function compare_output() {
+    test_name="$1"
+    out_name="$2"
+    output_file="$3"
+    expected_file="$4"
+
+    diff_out="$(diff --unified=2 --new-file "$expected_file" "$output_file")"
+    if [ $? -ne 0 ]; then
+        echo -e "${c_red}[Fail]${c_clear} wrong $out_name for $test_name"
+        echo "$diff_out"
+
+        return 1
+    fi
+
+    return 0
+}
+
+# keep track of executed and passed tests
 test_count=0
 pass_count=0
-
-timeout_after=0.5
 
 # Iterate over every *.fish, executing with input from *.stdin (if it exists).
 # The standard output and error of the script are then compared to *.stdout
@@ -29,78 +51,42 @@ do
     (( test_count++ ))
 
     base=${script%.*}
-    name=$(basename $base)
-    stdin=$base.stdin
-    expected_stdout=$base.stdout
-    expected_stderr=$base.stderr
+    name=$(basename "$base")
+    stdin="$base.stdin"
+    expected_stdout="$base.stdout"
+    expected_stderr="$base.stderr"
 
     echo "Testing $name..."
 
     if [ -f $stdin ]; then
         timeout $timeout_after $test_target $script <$stdin >$stdout 2>$stderr
     else
-        timeout $timeout_after $test_target $script <$empty >$stdout 2>$stderr
+        timeout $timeout_after $test_target $script </dev/null >$stdout 2>$stderr
     fi
 
+    echo -ne "\e[F\e[2K"
+
     if [ $? -eq 124 ]; then
-        echo -ne "\e[F\e[2K"
         echo -e "${c_red}[Fail]${c_clear} timeout for $name"
         continue
     fi
 
-    # if there's are no files, assume they should be empty
-    if ! [ -f $expected_stdout ]; then
-        expected_stdout=$empty
-    fi
-    if ! [ -f $expected_stderr ]; then
-        expected_stderr=$empty
-    fi
-
     fail=no
+    compare_output "$name" stdout "$stdout" "$expected_stdout" || fail=yes
+    compare_output "$name" stderr "$stderr" "$expected_stderr" || fail=yes
 
-    cmp $stdout $expected_stdout &>/dev/null
-    if [ $? -ne 0 ]; then
-        echo -ne "\e[F\e[2K"
-
-        echo -e "${c_red}[Fail]${c_clear} wrong stdout for $name"
-        echo "--- Expected: ---"
-        cat $expected_stdout
-        echo "--- Actual: -----"
-        cat $stdout
-        echo "-----------------"
-
-        fail=yes
-    fi
-
-    cmp $stderr $expected_stderr &>/dev/null
-    if [ $? -ne 0 ]; then
-        if ! [ "$fail" = yes ]; then
-            echo -ne "\e[F\e[2K"
-        fi
-
-        echo -e "${c_red}[Fail]${c_clear} wrong stderr for $name"
-        echo "--- Expected: ---"
-        cat $expected_stderr
-        echo "--- Actual: -----"
-        cat $stderr
-        echo "-----------------"
-
-        fail=yes
-    fi
-
-    if ! [ "$fail" = yes ]; then
+    if [ "$fail" = no ]; then
         (( pass_count++ ))
-        echo -ne "\e[F\e[2K" # Back up and erase one line
         echo -e "${c_green}[Pass]${c_clear} $name"
     fi
 done
 
-rm $stdout $stderr $empty
+rm $stdout $stderr
 
 if [ $test_count = $pass_count ]; then
-    echo -e "${bold}${c_green}${pass_count}/${test_count} tests passed${c_clear}"
+    echo -e "${c_bold}${c_green}${pass_count}/${test_count} tests passed${c_clear}"
     exit 0
 else
-    echo -e "${bold}${c_red}${pass_count}/${test_count} tests passed${c_clear}"
+    echo -e "${c_bold}${c_red}${pass_count}/${test_count} tests passed${c_clear}"
     exit 1
 fi
